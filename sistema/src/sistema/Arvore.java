978fe294630f65aca7ae96460d6bd6276bbee977
Matheus Samá <matheussama14@hotmail.com>
2026-06-09 16:54:32 -0300

    melhorias

--- sistema/src/sistema/Arvore.java
+++ sistema/src/sistema/Arvore.java
@@ -3,4 +3,13 @@ package sistema;
 public class Arvore {
     No raiz = null;
     int tamanho = 0;
+
+    public void inserir(Paciente paciente, No lugar){
+        if(lugar == null){
+            System.out.println(" "+paciente.toString());
+            raiz = new No(paciente);
+        }else if(paciente.getProtuario() < lugar.paciente.getProtuario()){
+            
+        }
+    }
 }
--- sistema/src/sistema/No.java
+++ sistema/src/sistema/No.java
@@ -4,4 +4,10 @@ public class No {
     Paciente paciente;
     No esquerda;
     No direita;
+
+    public No(Paciente paciente){
+        this.paciente = paciente;
+        this.esquerda = null;
+        this.direita = null;
+    }
 }
