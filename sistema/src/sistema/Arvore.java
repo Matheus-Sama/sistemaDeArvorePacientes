fe35fda4d4724bbe5e57e5a7677e8848c2c6d546
Matheus Samá <matheussama14@hotmail.com>
2026-06-09 17:09:51 -0300

    inserindo novos métodos

--- sistema/src/sistema/Arvore.java
+++ sistema/src/sistema/Arvore.java
@@ -8,8 +8,33 @@ public class Arvore {
         if(lugar == null){
             System.out.println(" "+paciente.toString());
             raiz = new No(paciente);
+            tamanho++;
         }else if(paciente.getProtuario() < lugar.paciente.getProtuario()){
-            
+            if(lugar.esquerda == null){
+                System.out.println(" "+paciente.toString());
+                lugar.esquerda = new No(paciente);
+                tamanho++;
+            }else{
+                inserir(paciente, lugar.esquerda);
+            }
+        }else if(paciente.getProtuario() > lugar.paciente.getProtuario()){
+            if(lugar.direita == null){
+                System.out.println(" "+ paciente.toString());
+                lugar.direita = new No(paciente);
+                tamanho ++;
+            }else{
+                inserir(paciente, lugar.direita);
+            }
+        }
+    }
+
+    public void preOrdem(No lugar){
+        System.out.println(" " + lugar.paciente.toString());
+        if(lugar.esquerda != null){
+            preOrdem(lugar.esquerda);
+        }
+        if(lugar.direita != null){
+            preOrdem(lugar.direita);
         }
     }
 }
