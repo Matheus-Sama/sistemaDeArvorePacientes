825363035f9afdacd69cc8fc9d9c58ffcbd7613b
Matheus Samá <128182849+Matheus-Sama@users.noreply.github.com>
2026-06-10 08:13:33 -0300

    Corrigindo erros

--- sistema/src/sistema/Arvore.java
+++ sistema/src/sistema/Arvore.java
@@ -62,6 +62,10 @@ public class Arvore {
     }
 
     public void buscarProtuario(No lugar, int protuario){
+        
+        if(lugar.paciente.getProtuario() == protuario){
+            System.out.prinln(lugar.paciente.toString());
+        }
         if(lugar.esquerda != null){
             if(lugar.paciente.getProtuario() == protuario){
                 System.out.println(lugar.paciente.toString());
